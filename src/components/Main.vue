db28dca49df2c6759fe6422cfa9ac7e286e501f4
Balis, John Ulysses George <jbalis@usgs.gov>
2019-06-13 14:06:15 -0500

    formatted main.vue and removed dead code

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,238 +1,157 @@
 <template>
-  <div>
- <head>
-    <title>USGS NWIS Tableau Data Connector</title>
-    <meta http-equiv="Cache-Control" content="no-store" />
-    
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-   
-</head>
+    <div>
 
-<body>
-    <div class="container container-table">
-        <div class="row vertical-center-row">
-            <div class="text-center col-md-4 col-md-offset-4">
-                <br>
-                <label> Site or Sites </label>
-                <input v-model="sites" placeholder="edit me">
-                <br>
-                <label> Parameter Codes</label>
-                <input v-model="parameters" placeholder="edit me">
-                <br>
-                <button type = "button" v-on:click = "requestData" id = "submitButton" class = "btn btn-success" style = "margin: 10px;">Request Data</button>
+        <head>
+            <title>USGS NWIS Tableau Data Connector</title>
+            <meta http-equiv="Cache-Control" content="no-store" />
+
+        </head>
+
+        <body>
+            <div class="container container-table">
+                <div class="row vertical-center-row">
+                    <div class="text-center col-md-4 col-md-offset-4">
+                        <br>
+                        <label> Site or Sites </label>
+                        <input v-model="sites" placeholder="edit me">
+                        <br>
+                        <label> Parameter Codes</label>
+                        <input v-model="parameters" placeholder="edit me">
+                        <br>
+                        <button type="button" v-on:click="requestData" id="submitButton" class="btn btn-success"
+                            style="margin: 10px;">Request Data</button>
+                    </div>
+                </div>
             </div>
-        </div>
+        </body>
+
     </div>
-</body>
- 
-</div>
 </template>
 
 <script>
 export default {
-  name: 'Main',
-  props: {
-    msg: String
-  },
-  data: function() {
-      return {
-          columnList: [],
-          sites: "1",
-          parameters: "1"
-      }
-  },
-  created: function () {
-     this.initializeWebDataConnector();
-  },
-  methods: {   
-        requestData: function(){
+    name: 'Main',
+    props: {
+        msg: String
+    },
+    data: function () {
+        return {
+            columnList: [],
+            sites: "01646500",
+            parameters: "00060,00065"
+        }
+    },
+    created: function () {
+        this.initializeWebDataConnector();
+    },
+    methods: {
+        requestData: function () {
             //construct columnList
-        let paramList = this.parameters.replace(/\s/g, '').split(',');
-        let self = this;
-        self.columnList = [];
-        paramList.forEach(function (param){ // right now we are assuming there is only one site in the query
-            self.columnList.push(self.sites + '_' + param);
-        });
-          tableau.connectionData = {columnList:self.columnList, siteNums:self.sites, paramNums:self.parameters}; // here we send columnList, to be used in defining our schema
-          tableau.connectionName = "USGS Instantaneous Values Query"; // This will be the data source name in Tableau
-          tableau.submit(); // This sends the connector object to Tableau
-        },
-       initializeWebDataConnector: function(){
-
-
-    // mock ups of user inputs
-
-    // let sites = '01646500'
-    // let parameters = '00060,00065'
-    // let sites = this.sites;
-    // let parameters = this.parameters;
-
-    //  //construct columnList
-    //     let paramList = parameters.replace(/\s/g, '').split(',');
-    //     let self = this;
-    //     self.columnList = [];
-    //     paramList.forEach(function (param){ // right now we are assuming there is only one site in the query
-    //         self.columnList.push(sites + '_' + param);
-    //     });
-
-
-
-
-         let myConnector = tableau.makeConnector();
-
-
-
-    // Define the schema
-    myConnector.getSchema = function(schemaCallback) {
-        /*
-        let cols = [{
-            id: "id",
-            dataType: tableau.dataTypeEnum.string
-        }, {
-            id: "mag",
-            alias: "magnitude",
-            dataType: tableau.dataTypeEnum.float
-        }, {
-            id: "title",
-            alias: "title",
-            dataType: tableau.dataTypeEnum.string
-        }, {
-            id: "test",
-            alias: "test",
-            dataType: tableau.dataTypeEnum.string
-        },{
-            id: "location",
-            dataType: tableau.dataTypeEnum.geometry
-        }];
-        */
-
-        let cols = [];
-        /*
-        cols.push({
-                id: 'id',
-                dataType: tableau.dataTypeEnum.string
-        });
-        */
-        tableau.connectionData.columnList.forEach(function (column){ // we add all the columns to the schema
-            cols.push({
-                id: column,
-                alias: column,
-                dataType: tableau.dataTypeEnum.string
+            let paramList = this.parameters.replace(/\s/g, '').split(',');
+            let self = this;
+            self.columnList = [];
+            paramList.forEach(function (param) { // right now we are assuming there is only one site in the query
+                self.columnList.push(self.sites + '_' + param);
             });
-        });
+            tableau.connectionData = { columnList: self.columnList, siteNums: self.sites, paramNums: self.parameters }; // here we send columnList, to be used in defining our schema
+            tableau.connectionName = "USGS Instantaneous Values Query"; // This will be the data source name in Tableau
+            tableau.submit(); // This sends the connector object to Tableau
+        },
+        initializeWebDataConnector: function () {
+            let myConnector = tableau.makeConnector();
 
+            // Define the schema
+            myConnector.getSchema = function (schemaCallback) {
 
-        let tableSchema = {
-            id: "WaterData",
-            alias: "useful information will be put here", //todo, add useful information
-            columns: cols
-        };
-
-        //alert(JSON.stringify(tableSchema));
-
-        schemaCallback([tableSchema]);
-    };
-
-
-
-
-    // Download the data
-    myConnector.getData = function(table, doneCallback) {
-    // adapted from https://github.com/usgs/waterdataui/blob/master/assets/src/scripts/ajax.js
-    let get = function (url) {
-    // Return a new promise.
-    return new Promise(function(resolve, reject) {
-        // Do the usual XHR stuff
-        let req = new XMLHttpRequest();
-        req.responseType = 'json';
-
-        req.open('GET', url);
-
-        req.onload = function() {
-            // This is called even on 404 etc
-            // so check the status
-            if (req.status == 200) {
-                 let data = req.response;
-                 let tableData = [];
-
-                  console.log(JSON.stringify(data));
-                 let dataLength = data.value.timeSeries[0].values[0].value.length;
-
-                    for(let i = 0; i < dataLength; i++)
-                    {
-                        let newEntry = {};
-                        //newEntry['id'] = String(i);
-                        for(let c = 0; c < tableau.connectionData.columnList.length; c++)
-                        {
-                            
-                           newEntry[tableau.connectionData.columnList[c]] = data.value.timeSeries[c].values[0].value[i].value;
-                              
-                        }
-                        tableData.push(newEntry);
-                    }
-                    console.log(JSON.stringify(tableData))
-
-                /*
-
-                // Iterate over the JSON object
-                for (let i = 0, len = feat.length; i < len; i++) {
-                    tableData.push({
-                        "id": feat[i].id,
-                        "mag": feat[i].properties.mag,
-                        "title": feat[i].properties.title,
-                        "location": feat[i].geometry
+                let cols = [];
+                tableau.connectionData.columnList.forEach(function (column) { // we add all the columns to the schema
+                    cols.push({
+                        id: column,
+                        alias: column,
+                        dataType: tableau.dataTypeEnum.string
                     });
-                }
-
-                */
+                });
 
 
-
-            table.appendRows(tableData);
-            doneCallback();
-
-                // Resolve the promise with the response text
-                resolve(req.response);
-            } else {
-                // Otherwise reject with the status text
-                // which will hopefully be a meaningful error
-                if (window.ga) {
-                    window.ga('send', 'event', 'serviceFailure', req.status, url);
-                }
-                reject(Error(`Failed with status ${req.status}: ${req.statusText}`));
-            }
-        };
-
-        // Handle network errors
-        req.onerror = function() {
-            reject(Error('Network Error'));
-        };
-
-        // Make the request
-        req.send();
-    });
-};
-
-        let paramList = tableau.connectionData.paramNums.replace(/\s/g, '').split(',');
-
-    let url =  `https://waterservices.usgs.gov/nwis/iv/?format=json&sites=${tableau.connectionData.siteNums}&period=P1D&parameterCd=${paramList.join()}&siteStatus=all`
-    console.log(url);
-
-   //let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson"
+                let tableSchema = {
+                    id: "WaterData",
+                    alias: "useful information will be put here", //todo, add useful information
+                    columns: cols
+                };
+                schemaCallback([tableSchema]);
+            };
 
 
 
 
-    get(url);};
+            // Download the data
+            myConnector.getData = function (table, doneCallback) {
+                // adapted from https://github.com/usgs/waterdataui/blob/master/assets/src/scripts/ajax.js
+                let get = function (url) {
+                    // Return a new promise.
+                    return new Promise(function (resolve, reject) {
+                        // Do the usual XHR stuff
+                        let req = new XMLHttpRequest();
+                        req.responseType = 'json';
 
-    tableau.registerConnector(myConnector);
-    
-       } 
+                        req.open('GET', url);
+
+                        req.onload = function () {
+                            // This is called even on 404 etc
+                            // so check the status
+                            if (req.status == 200) {
+                                let data = req.response;
+                                let tableData = [];
+                                let dataLength = data.value.timeSeries[0].values[0].value.length;
+
+                                for (let i = 0; i < dataLength; i++) {
+                                    let newEntry = {};
+                                    for (let c = 0; c < tableau.connectionData.columnList.length; c++) {
+
+                                        newEntry[tableau.connectionData.columnList[c]] = data.value.timeSeries[c].values[0].value[i].value;
+
+                                    }
+                                    tableData.push(newEntry);
+                                }
+
+                                table.appendRows(tableData);
+                                doneCallback();
+
+                                // Resolve the promise with the response text
+                                resolve(req.response);
+                            } else {
+                                // Otherwise reject with the status text
+                                // which will hopefully be a meaningful error
+                                if (window.ga) {
+                                    window.ga('send', 'event', 'serviceFailure', req.status, url);
+                                }
+                                reject(Error(`Failed with status ${req.status}: ${req.statusText}`));
+                            }
+                        };
+
+                        // Handle network errors
+                        req.onerror = function () {
+                            reject(Error('Network Error'));
+                        };
+
+                        // Make the request
+                        req.send();
+                    });
+                };
+
+
+
+                //todo standardize this template's format when we add more query info fields
+                let paramList = tableau.connectionData.paramNums.replace(/\s/g, '').split(',');
+                let url = `https://waterservices.usgs.gov/nwis/iv/?format=json&sites=${tableau.connectionData.siteNums}&period=P1D&parameterCd=${paramList.join()}&siteStatus=all`
+                get(url);
+            };
+            tableau.registerConnector(myConnector);
+
+        }
     }
 }
-
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 h3 {
