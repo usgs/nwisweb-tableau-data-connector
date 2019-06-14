3b1d57ec1b5584187f820db779e45f144a6d3b3d
Sreenivasam, Ryan Rakesh <rsreenivasam@usgs.gov>
2019-06-14 12:41:33 -0500

    added url test

--- tests/unit/getData.spec.js
+++ tests/unit/getData.spec.js
@@ -2,7 +2,7 @@ import { shallowMount } from '@vue/test-utils'
 
 import Main from '@/components/Main.vue'
 
-import {getData, getSchema} from '../../src/components/WDCMethods.js'
+import {getData, getSchema, generateURL} from '../../src/components/WDCMethods.js'
 
 const fake_url = 'https://fake.service.com/request';
 let table = {};
@@ -21,4 +21,17 @@ it('The url passed to the get command is used to make a request', () => {
   // expect(thisRequest.url).toBe(FAKE_SERVICE);
   // expect(thisRequest.method).toBe('GET');
 });
+
+it('Empty url is returned if no sites are specified', () => {
+  let connectionData = {paramNums:"", siteNums:""};
+  let url = generateURL(connectionData);
+  expect(url).toContain("https://waterservices.usgs.gov/nwis/iv/?format=json&sites=&period=P1D&parameterCd=&siteStatus=all");
+});
+
+it('Correct url is returned if 1 site and 1 parameter are specified', () => {
+  let connectionData = {siteNums:"01646500", paramNums:"00060"};
+  let url = generateURL(connectionData);
+  expect(url).toContain("https://waterservices.usgs.gov/nwis/iv/?format=json&sites=01646500&period=P1D&parameterCd=00060&siteStatus=all");
+});
+
 })
