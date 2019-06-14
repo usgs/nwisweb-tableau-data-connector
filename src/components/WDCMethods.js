bbde937b6d153886331ec47512ac2d3c293d55de
Balis, John Ulysses George <jbalis@usgs.gov>
2019-06-14 10:48:57 -0500

    refactored WDCMethods for modularity

--- src/components/WDCMethods.js
+++ src/components/WDCMethods.js
@@ -1,3 +1,39 @@
+
+
+function formatJSONAsTable (data){
+    let tableData = [];
+    let timeSeries = data.value.timeSeries;
+    let dataIndices = Array.from(timeSeries[0].values[0].value.keys());
+    let paramIndices = Array.from(timeSeries.keys());
+
+    dataIndices.forEach(i => {
+        let newEntry = {};
+        paramIndices.forEach(c => {
+            try{
+                let name = timeSeries[c].name;
+                let nameTokens = name.split(':');
+                let site = nameTokens[1];
+                let paramType = nameTokens[2];
+                newEntry[site + '_' + paramType] = data.value.timeSeries[c].values[0].value[i].value;
+            }catch(err)
+            {
+                //ignore index(out of range for this parameter for this site)
+            }
+        });
+        tableData.push(newEntry); 
+    });
+    return tableData;
+}
+
+
+function generateURL(connectionData){
+ //todo standardize this template's format when we add more query info fields
+ let paramList = connectionData.paramNums.replace(/\s/g, '').split(','); // split by comma, ignoring whitespace
+ let siteList = connectionData.siteNums.replace(/\s/g,'').split(',');
+ return  `https://waterservices.usgs.gov/nwis/iv/?format=json&sites=${siteList.join()}&period=P1D&parameterCd=${paramList.join()}&siteStatus=all`
+}
+
+
 function getData (table, doneCallback) {
         // adapted from https://github.com/usgs/waterdataui/blob/master/assets/src/scripts/ajax.js
         let get = function (url) {
@@ -38,32 +74,10 @@ function getData (table, doneCallback) {
       
 
 
-        //todo standardize this template's format when we add more query info fields
-        let paramList = tableau.connectionData.paramNums.replace(/\s/g, '').split(','); // split by comma, ignoring whitespace
-        let siteList = tableau.connectionData.siteNums.replace(/\s/g,'').split(',');
-        let url = `https://waterservices.usgs.gov/nwis/iv/?format=json&sites=${siteList.join()}&period=P1D&parameterCd=${paramList.join()}&siteStatus=all`
+       let url = generateURL(tableau.connectionData);
        
        get(url).then(function(value){ 
-                        let data = value;
-                        let tableData = [];
-                        //let columnIndices = Array.from(tableau.connectionData.columnList.keys());
-                        let timeSeries = data.value.timeSeries;
-                        let dataIndices = Array.from(timeSeries[0].values[0].value.keys());
-                        let paramIndices = Array.from(timeSeries.keys());
-
-                        dataIndices.forEach(i => {
-                            let newEntry = {};
-                            paramIndices.forEach(c => {
-                                let name = timeSeries[c].name;
-                                let nameTokens = name.split(':');
-                                let site = nameTokens[1];
-                                let paramType = nameTokens[2];
-                                newEntry[site + '_' + paramType] = data.value.timeSeries[c].values[0].value[i].value;
-                            });
-                            tableData.push(newEntry); 
-                        });
-
-                        table.appendRows(tableData);
+                        table.appendRows(formatJSONAsTable(value));
         });
     }
 
