f29a4abb0d950545337424360701afc5554cd889
Balis, John Ulysses George <jbalis@usgs.gov>
2019-06-14 09:41:01 -0500

    refactored web data connector functions to be in separate module

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -29,6 +29,7 @@
 </template>
 
 <script>
+import { getData, getSchema } from './WDCMethods.js';
 export default {
     name: 'Main',
     props: {
@@ -61,92 +62,10 @@ export default {
             let myConnector = tableau.makeConnector();
 
             // Define the schema
-            myConnector.getSchema = function (schemaCallback) {
-
-                let cols = [];
-                tableau.connectionData.columnList.forEach(function (column) { // we add all the columns to the schema
-                    cols.push({
-                        id: column,
-                        alias: column,
-                        dataType: tableau.dataTypeEnum.string
-                    });
-                });
-
-
-                let tableSchema = {
-                    id: "WaterData",
-                    alias: "useful information will be put here", //todo, add useful information
-                    columns: cols
-                };
-                schemaCallback([tableSchema]);
-            };
-
-
-
+            myConnector.getSchema = getSchema;
 
             // Download the data
-            myConnector.getData = function (table, doneCallback) {
-                // adapted from https://github.com/usgs/waterdataui/blob/master/assets/src/scripts/ajax.js
-                let get = function (url) {
-                    // Return a new promise.
-                    return new Promise(function (resolve, reject) {
-                        // Do the usual XHR stuff
-                        let req = new XMLHttpRequest();
-                        req.responseType = 'json';
-
-                        req.open('GET', url);
-
-                        req.onload = function () {
-                            // This is called even on 404 etc
-                            // so check the status
-                            if (req.status == 200) {
-                                doneCallback();
-                                // Resolve the promise with the response text
-                                resolve(req.response);
-                            } else {
-                                // Otherwise reject with the status text
-                                // which will hopefully be a meaningful error
-                                if (window.ga) {
-                                    window.ga('send', 'event', 'serviceFailure', req.status, url);
-                                }
-                                reject(Error(`Failed with status ${req.status}: ${req.statusText}`));
-                            }
-                        };
-
-                        // Handle network errors
-                        req.onerror = function () {
-                            reject(Error('Network Error'));
-                        };
-
-                        // Make the request
-                        req.send();
-                    });
-                };
-              
-
-
-                //todo standardize this template's format when we add more query info fields
-                let paramList = tableau.connectionData.paramNums.replace(/\s/g, '').split(',');
-                let url = `https://waterservices.usgs.gov/nwis/iv/?format=json&sites=${tableau.connectionData.siteNums}&period=P1D&parameterCd=${paramList.join()}&siteStatus=all`
-               
-               get(url).then(function(value){ 
-                                let data = value;
-                                let tableData = [];
-                                let columnIndices = Array.from(tableau.connectionData.columnList.keys());
-                                let dataIndices = Array.from(data.value.timeSeries[0].values[0].value.keys());
-
-                                dataIndices.forEach(i => {
-                                    let newEntry = {};
-                                    columnIndices.forEach(c => {
-                                        newEntry[tableau.connectionData.columnList[c]] = data.value.timeSeries[c].values[0].value[i].value;
-
-                                    });
-                                    tableData.push(newEntry); 
-                                });
-
-                                table.appendRows(tableData);
-                });
-            };
+            myConnector.getData = getData;
             tableau.registerConnector(myConnector);
 
         }
