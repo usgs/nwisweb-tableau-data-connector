3b61abde14e23686d74198de851fa4f5db16c428
Balis, John Ulysses George <jbalis@usgs.gov>
2019-06-19 12:57:59 -0500

    changes to docker files

--- control.sh
+++ control.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 
 docker build . --tag="test"
--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -2,11 +2,5 @@
 
 
 cd usr/local/bin/nwisweb-tableau-data-connector
-if npm run test:unit; then
-    npm run build
-    echo "built!"
-else
-    echo "failed!"
-fi
-
+npm run production-build
 
