4b739cb912a3da5382616aea9674458ebe9ffdfa
Balis, John Ulysses George <jbalis@usgs.gov>
2019-06-20 10:34:52 -0500

    linted most of the source code

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,3 @@
-
 /*
 gets a JSON from specified URL via HTTP request
 
@@ -6,38 +5,38 @@ gets a JSON from specified URL via HTTP request
 
 const get = (url, responseType) => {
   // Return a new promise.
-  return new Promise(function (resolve, reject) {
-      // Do the usual XHR stuff
-      let req = new XMLHttpRequest();
-      req.responseType = responseType;
+  return new Promise(function(resolve, reject) {
+    // Do the usual XHR stuff
+    let req = new XMLHttpRequest();
+    req.responseType = responseType;
 
-      req.open('GET', url);
+    req.open("GET", url);
 
-      req.onload = function () {
-          // This is called even on 404 etc
-          // so check the status
-          if (req.status == 200) {
-              // Resolve the promise with the response text
-              resolve(req.response);
-          } else {
-              // Otherwise reject with the status text
-              // which will hopefully be a meaningful error
-              if (window.ga) {
-                  window.ga('send', 'event', 'serviceFailure', req.status, url);
-              }
-              alert(`Failed with status ${req.status}: ${req.statusText}`);
-              reject(Error(`Failed with status ${req.status}: ${req.statusText}`));
-          }
-      };
+    req.onload = function() {
+      // This is called even on 404 etc
+      // so check the status
+      if (req.status == 200) {
+        // Resolve the promise with the response text
+        resolve(req.response);
+      } else {
+        // Otherwise reject with the status text
+        // which will hopefully be a meaningful error
+        if (window.ga) {
+          window.ga("send", "event", "serviceFailure", req.status, url);
+        }
+        alert(`Failed with status ${req.status}: ${req.statusText}`);
+        reject(Error(`Failed with status ${req.status}: ${req.statusText}`));
+      }
+    };
 
-      // Handle network errors
-      req.onerror = function () {
-          reject('Network Error');
-      };
+    // Handle network errors
+    req.onerror = function() {
+      reject("Network Error");
+    };
 
-      // Make the request
-      req.send();
+    // Make the request
+    req.send();
   });
-}
+};
 
-export{get};
+export { get };
