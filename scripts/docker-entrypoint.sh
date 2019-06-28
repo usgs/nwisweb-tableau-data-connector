b5e86be4be29330a4693b7bbf4bb5ba7110dac8b
Sreenivasam, Ryan Rakesh <rsreenivasam@usgs.gov>
2019-06-28 13:46:05 -0500

    another autoversioning test

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -6,9 +6,10 @@ versionNum=`cat versionNum.txt`
 cat versionNum.txt
 cd usr/local/bin/nwisweb-tableau-data-connector
 npm install
+npm version patch
+
 git add .
 git commit -m "automated version number update"
 git push origin autoversion-test
-npm version patch
 npm run production-build
 
