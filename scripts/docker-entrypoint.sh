185a463f00facdafa092007c2972e327e1b66e7e
Sreenivasam, Ryan Rakesh <rsreenivasam@usgs.gov>
2019-07-01 16:31:59 -0500

    jenkins version number testing

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -2,6 +2,7 @@
 
 cd usr/local/bin/nwisweb-tableau-data-connector
 versionNum=`cat versionNum.txt`
+cat versionNum.txt
 npm install
 npm --no-git-tag-version version $versionNum --force
 npm run production-build
