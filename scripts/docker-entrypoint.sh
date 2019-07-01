fc3a56bf090c6eb411aed647edd23f1f16a8f884
Sreenivasam, Ryan Rakesh <rsreenivasam@usgs.gov>
2019-07-01 17:49:11 -0500

    testing for tags

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -2,7 +2,6 @@
 
 cd usr/local/bin/nwisweb-tableau-data-connector
 versionType=`cat versionNum.txt`
-cat versionNum.txt
 npm install
 npm --no-git-tag-version version $versionType --force >newVerNum.txt
 npm run production-build
