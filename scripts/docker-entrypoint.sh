d6b3a14e5c202dec128b7e2c189a33200ecbccc5
Sreenivasam, Ryan Rakesh <rsreenivasam@usgs.gov>
2019-07-01 15:58:02 -0500

    jenkins version number testing

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -6,6 +6,6 @@ ls -al
 versionNum=`cat versionNum.txt`
 cat versionNum.txt
 npm install
-npm --no-git-tag-version version patch --force
+npm --no-git-tag-version version $versionNum --force
 npm run production-build
 
