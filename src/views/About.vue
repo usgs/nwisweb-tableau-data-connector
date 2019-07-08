115a516910b15d86b232c3edd6ebd764a25219a5
Balis, John Ulysses George <jbalis@usgs.gov>
2019-07-08 15:57:06 -0500

    updated changelog

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,26 @@
 <template>
   <div class="about">
-    <b> How to use the NWISWeb Tableau Web Data Connector</b>
+    <b> How to use the NWISWeb Tableau Web Data Connector</b> <br />
+
+    This system requires a desktop tableau client. To use this system, install a
+    tableau desktop client on your PC then enter the URL for this page under the
+    web data connector option in the connect menu. Details of the constraints
+    imposed by the database that feeds this tool are available
+    <a href="https://waterservices.usgs.gov/rest/IV-Service.html">here</a>.
+
+    <br />
+    <br />
+    <b> Who is this system designed for </b> <br />
+    This system is designed for users of the Tableau software who want to
+    seamlessly integrate USGS Water data into their analytics operations. This
+    system is open to the public.
+
+    <br />
+    <br />
+    <b> What does this system provide </b> <br />
+    This system provides USGS Water data and an accompanying Tableau schema for
+    each parameter for each site, including geographic coordinates and datetimes
+    for each observation. When invoking this system through the Tableau web data
+    connector option, the data is automatically loaded into Tableau.
   </div>
 </template>
