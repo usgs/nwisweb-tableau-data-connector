c63dc97239447978811049dcc657c8545ddbbcda
Sreenivasam, Ryan Rakesh <rsreenivasam@usgs.gov>
2019-07-24 16:57:59 -0500

    added currVerNum for captured current version number

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 cd usr/local/bin/nwisweb-tableau-data-connector
-versionType=`cat versionNum.txt`
+versionType=`cat versionType.txt`
 npm install
-npm --no-git-tag-version version $versionType --force >newVerNum.txt
+npm ls --depth 0 --json | jq '.version' > currVerNum.txt
+npm --no-git-tag-version version $versionType --force > newVerNum.txt
 npm run production-build
