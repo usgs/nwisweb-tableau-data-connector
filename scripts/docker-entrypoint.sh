1b9a45435e9e4766e50681b70ac5eabaf3416b4e
Sreenivasam, Ryan Rakesh <rsreenivasam@usgs.gov>
2019-07-25 15:16:12 -0500

    added script to get package version

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -3,6 +3,6 @@
 cd usr/local/bin/nwisweb-tableau-data-connector
 versionType=`cat versionType.txt`
 npm install
-npm ls --depth 0 --json | jq '.version' > currVerNum.txt
+npm --loglevel silent run vars > currVerNum.txt
 npm --no-git-tag-version version $versionType --force > newVerNum.txt
 npm run production-build
