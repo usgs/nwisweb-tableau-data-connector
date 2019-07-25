e30296da7ea7aa70a435178bff09ea57d120bb5d
Sreenivasam, Ryan Rakesh <rsreenivasam@usgs.gov>
2019-07-25 13:31:07 -0500

    jenkins testing

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -3,6 +3,7 @@
 cd usr/local/bin/nwisweb-tableau-data-connector
 versionType=`cat versionType.txt`
 npm install
+apt-get install jq
 npm ls --depth 0 --json | jq '.version' > currVerNum.txt
 npm --no-git-tag-version version $versionType --force > newVerNum.txt
 npm run production-build
