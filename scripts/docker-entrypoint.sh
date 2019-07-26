44c9f8cad1be54c47c67e070c02f016d5b8c6ddd
Sreenivasam, Ryan Rakesh <rsreenivasam@usgs.gov>
2019-07-26 09:53:49 -0500

    removed test.txt and reverted version number

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -4,5 +4,5 @@ cd usr/local/bin/nwisweb-tableau-data-connector
 versionType=`cat versionType.txt`
 npm install
 npm ls --depth 0 --json | jq '.version' | sed -e 's/^"//' -e 's/"$//' > currVerNum.txt
-npm --no-git-tag-version version $versionType --force
+npm --no-git-tag-version version $versionType --force > newVerNum.txt
 npm run production-build
