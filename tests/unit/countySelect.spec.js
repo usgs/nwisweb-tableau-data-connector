3e82919b05a78f837bd7a57ece03116b6f0a481e
Sreenivasam, Ryan Rakesh <rsreenivasam@usgs.gov>
2019-07-29 13:03:15 -0500

    added several unit tests for getCounties

---
+++ tests/unit/countySelect.spec.js
@@ -0,0 +1,67 @@
+import { shallowMount, createLocalVue } from "@vue/test-utils";
+import CountySelect from "../../src/components/CountySelect.vue";
+import Vuex from "vuex";
+import Notifications from "vue-notification";
+import VueTags from "vue-tags";
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
+localVue.use(Notifications);
+
+localVue.component("input-tags", VueTags);
+
+describe("CountySelect", () => {
+  let store;
+  test("test getCounties with valid state name", () => {
+    store = new Vuex.Store({
+      state: {},
+      modules: {},
+      getters: {},
+      actions: {}
+    });
+    const wrapper = shallowMount(CountySelect, {
+      store,
+      localVue,
+      propsData: {}
+    });
+    let counties = [
+      {"country_cd": "US", "county_cd": "001", "county_nm": "Bristol County", "state_cd": "44"}, 
+      {"country_cd": "US", "county_cd": "003", "county_nm": "Kent County", "state_cd": "44"}, 
+      {"country_cd": "US", "county_cd": "005", "county_nm": "Newport County", "state_cd": "44"}, 
+      {"country_cd": "US", "county_cd": "007", "county_nm": "Providence County", "state_cd": "44"}, 
+      {"country_cd": "US", "county_cd": "009", "county_nm": "Washington County", "state_cd": "44"}
+    ];
+    expect(wrapper.vm.getCounties("Rhode Island")).toEqual(counties);
+  });
+
+//   test("test getCounties with invalid state name", () => {
+//     store = new Vuex.Store({
+//       state: {},
+//       modules: {},
+//       getters: {},
+//       actions: {}
+//     });
+//     const wrapper = shallowMount(CountySelect, {
+//       store,
+//       localVue,
+//       propsData: {}
+//     });
+//     expect(wrapper.vm.getCounties("Mayonnaise")).toEqual([]);
+//   });
+
+//   test("test getCountyNameFromCode", () => {
+//     store = new Vuex.Store({
+//       state: {},
+//       modules: {},
+//       getters: {},
+//       actions: {}
+//     });
+//     const wrapper = shallowMount(CountySelect, {
+//       store,
+//       localVue,
+//       propsData: {}
+//     });
+//     let countyCode = "12003";
+//     expect(wrapper.vm.getCountyNameFromCode(countyCode)).toEqual("Baker County");
+//   });
+});
