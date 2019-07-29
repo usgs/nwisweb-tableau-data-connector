b0d4c36d413b871ac17defe7189636b158ac42fd
Sreenivasam, Ryan Rakesh <rsreenivasam@usgs.gov>
2019-07-29 13:58:44 -0500

    added aquiferInputs test file

--- tests/unit/aquiferInputs.spec.js
+++ tests/unit/aquiferInputs.spec.js
@@ -1,5 +1,5 @@
 import { shallowMount, createLocalVue } from "@vue/test-utils";
-import CountySelect from "../../src/components/CountySelect.vue";
+import AquiferInputs from "../../src/components/AquiferInputs.vue";
 import Vuex from "vuex";
 import Notifications from "vue-notification";
 import VueTags from "vue-tags";
@@ -9,80 +9,80 @@ localVue.use(Vuex);
 localVue.use(Notifications);
 localVue.component("input-tags", VueTags);
 
-describe("getCounties", () => {
+describe("getLocAquifers", () => {
   let store;
-  test("test getCounties with valid state name", () => {
+  test("test getLocAquifers with valid state name", () => {
     store = new Vuex.Store({
       state: {},
       modules: {},
       getters: {},
       actions: {}
     });
-    const wrapper = shallowMount(CountySelect, {
+    const wrapper = shallowMount(AquiferInputs, {
       store,
       localVue,
       propsData: {},
       stubs: ["input-tags"]
     });
-    let counties = [
+    let locAquifers = [
       {"country_cd": "US", "county_cd": "001", "county_nm": "Bristol County", "state_cd": "44"}, 
       {"country_cd": "US", "county_cd": "003", "county_nm": "Kent County", "state_cd": "44"}, 
       {"country_cd": "US", "county_cd": "005", "county_nm": "Newport County", "state_cd": "44"}, 
       {"country_cd": "US", "county_cd": "007", "county_nm": "Providence County", "state_cd": "44"}, 
       {"country_cd": "US", "county_cd": "009", "county_nm": "Washington County", "state_cd": "44"}
     ];
-    expect(wrapper.vm.getCounties("Rhode Island")).toEqual(counties);
+    expect(wrapper.vm.getLocAquifers("Rhode Island")).toEqual(locAquifers);
   });
 
-  test("test getCounties with invalid state name", () => {
+  test("test getLocAquifers with invalid state name", () => {
     store = new Vuex.Store({
       state: {},
       modules: {},
       getters: {},
       actions: {}
     });
-    const wrapper = shallowMount(CountySelect, {
+    const wrapper = shallowMount(AquiferInputs, {
       store,
       localVue,
       propsData: {},
       stubs: ["input-tags"]
     });
-    expect(wrapper.vm.getCounties("Mayonnaise")).toEqual([]);
+    expect(wrapper.vm.getLocAquifers("Hamburger")).toEqual([]);
   });
 });
-describe("getCountyNameFromCode", () => {
+describe("getLocAqNameFromCode", () => {
   let store;
-  test("test getCountyNameFromCode with valid county code", () => {
+  test("test getLocAqNameFromCode with valid local aquifer code", () => {
     store = new Vuex.Store({
       state: {},
       modules: {},
       getters: {},
       actions: {}
     });
-    const wrapper = shallowMount(CountySelect, {
+    const wrapper = shallowMount(AquiferInputs, {
       store,
       localVue,
       propsData: {},
       stubs: ["input-tags"]
     });
     let countyCode = "12003";
-    expect(wrapper.vm.getCountyNameFromCode(countyCode)).toEqual("Baker County");
+    expect(wrapper.vm.getLocAqNameFromCode(countyCode)).toEqual("Baker County");
   });
 
-  test("test getCountyNameFromCode with invalid county code", () => {
+  test("test getLocAqNameFromCode with invalid local aquifer code", () => {
     store = new Vuex.Store({
       state: {},
       modules: {},
       getters: {},
       actions: {}
     });
-    const wrapper = shallowMount(CountySelect, {
+    const wrapper = shallowMount(AquiferInputs, {
       store,
       localVue,
       propsData: {},
       stubs: ["input-tags"]
     });
     let countyCode = "!!!!!";
-    expect(wrapper.vm.getCountyNameFromCode(countyCode)).toEqual("invalid");
+    expect(wrapper.vm.getLocAqNameFromCode(countyCode)).toEqual("invalid");
   });
 });
