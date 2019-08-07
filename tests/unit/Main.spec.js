a30d672b67ca038796a155d0c7b38f3201356c42
Balis, John Ulysses George <jbalis@usgs.gov>
2019-08-07 09:57:50 -0500

    added test

--- tests/unit/Main.spec.js
+++ tests/unit/Main.spec.js
@@ -47,6 +47,11 @@ let store;
 
 localVue.use(Vuex);
 
+let resultVal = "";
+exports.notify = input => {
+  resultVal = input;
+}; // checking to see what was notified
+
 test("Browser warning is succesfully issued when system is accessed outside of Tableau", () => {
   store = new Vuex.Store({
     state: {},
@@ -59,15 +64,32 @@ test("Browser warning is succesfully issued when system is accessed outside of T
     localVue
   });
 
-  let resultVal = "";
-  exports.notify = input => {
-    resultVal = input;
-  }; // checking to see what was notified
   const spy = jest.spyOn(exports, "notify");
   wrapper.vm.browserWarning();
 
   expect(spy).toHaveBeenCalled();
   expect(resultVal).toEqual(
     "The NWIS Tableau Web Data Connector must be accessed from Tableau desktop or Tableau server!"
+  );
+});
+
+test("loading warning is succesfully issued when user requests data prematurely", () => {
+  resultVal = "";
+  store = new Vuex.Store({
+    state: {},
+    modules: {},
+    getters: {},
+    actions: {}
+  });
+  const wrapper = shallowMount(Main, {
+    store,
+    localVue
+  });
+  wrapper.setData({ loadedStateData: false });
+  const spy = jest.spyOn(exports, "notify");
+  wrapper.vm.requestData();
+  expect(spy).toHaveBeenCalled();
+  expect(resultVal).toEqual(
+    "The page is still loading: please retry this action in a moment!"
   );
 });
